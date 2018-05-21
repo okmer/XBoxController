dfb7d3ac2bf370bdce8bbd6b089acd2a140125bc
Remko van Brakel <remko@okmer.com>
2018-05-21 22:47:11 +0200

    Moved the EventArgs types to a sub-folder.

---
+++ Com.Okmer.GameController/Components/EventArgs/StateChangeArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com.Okmer.GameController
+{
+    public class StateChangeArgs : EventArgs
+    {
+        public bool State { get; }
+
+        public StateChangeArgs(bool state)
+        {
+            State = state;
+        }
+    }
+}
